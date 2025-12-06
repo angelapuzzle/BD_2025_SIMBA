259c75471f154a1e8fc602ea0653e2bc88f4fe04
AngelaPuzzle <71221709+angelapuzzle@users.noreply.github.com>
2025-12-06 18:59:29 -0500

    "Finalizar" procedimientos Monitor
    Queda pendiente probarlos al crear la intefaz I guess

--- Scripts Consultas, PAs, Funciones y Triggers/Procedimientos y Funciones Monitor.sql
+++ Scripts Consultas, PAs, Funciones y Triggers/Procedimientos y Funciones Monitor.sql
@@ -4,11 +4,11 @@
 
 DELIMITER &&
 
--- Ver sesiones en curso (Vista 1)
+-- Ver sesiones en curso con filtro (Vista 1)
 DROP PROCEDURE IF EXISTS sp_verSesionesActuales &&
 CREATE PROCEDURE sp_verSesionesActuales(
     IN horaInicial time, IN idSala char(1), IN idComputador int,
-    IN tiunEstudiante bigint, IN nombreEstudiante varchar(45), IN apellidoEstudiante varchar(45)
+    IN tiunEstudiante bigint, IN nombreEstudiante varchar(45), IN apellidoEstudiante varchar(45),
     IN filtrarIncidentes smallint
 )
 BEGIN
@@ -25,11 +25,11 @@ BEGIN
         and (Ses_Fecha = CURRENT_DATE() and Ses_HoraFin is null);
 END &&
 
--- Ver historial de sesiones on filtro (Vista 1)
+-- Ver historial de sesiones con filtro (Vista 1)
 DROP PROCEDURE IF EXISTS sp_verSesionesHistorial &&
 CREATE PROCEDURE sp_verSesionesHistorial(
-    IN dia date, IN horaInicial time, IN horaFinal, time, IN idSala char(1), IN idComputador int,
-    IN tiunEstudiante bigint, IN nombreEstudiante varchar(45), IN apellidoEstudiante varchar(45)
+    IN dia date, IN horaInicial time, IN horaFinal time, IN idSala char(1), IN idComputador int,
+    IN tiunEstudiante bigint, IN nombreEstudiante varchar(45), IN apellidoEstudiante varchar(45),
     IN filtrarIncidentes smallint
 )
 BEGIN
@@ -47,13 +47,8 @@ BEGIN
         and (filtrarIncidentes is null or filtrarIncidentes = 0 or (Sal_Comentarios is not null and Sal_Comentarios != ''));
 END &&
 
-
-
-
 -- Ver estadísticas de uso por computador filtradas (Vista 5)       
-DROP PROCEDURE IF EXISTS sp_monVerEstadisticasComp;
-DELIMITER &&
-
+DROP PROCEDURE IF EXISTS sp_monVerEstadisticasComp &&
 CREATE PROCEDURE sp_monVerEstadisticasComp(
     IN idSala char(1), IN idComputador int
 )
@@ -64,23 +59,15 @@ BEGIN
         (Sal_Id=idSala)
         and (@computador is null or Com_Id=idcomputador);
 END &&
--- Valores de prueba
-CALL sp_monVerEstadisticasComp('A',1234)
 
 
--- ///////////////////////////////////////////////
-
--- Ver historial de uso de un computador (Vista 2)
--- Atributos: computador, sala, dias_atras (opcional)
-
-DROP PROCEDURE IF EXISTS sp_monVerHistorialPc;
-DELIMITER &&
+-- Ver historial de uso de un computador (Vista 1)
+DROP PROCEDURE IF EXISTS sp_monVerHistorialPc &&
 CREATE PROCEDURE sp_monVerHistorialPc(
 	IN idSala char(1), IN idComputador int,
     IN diasAtras int
 )
 BEGIN
-    
     SELECT 
         Ses_Fecha,
         Ses_HoraInicio,
@@ -96,20 +83,14 @@ BEGIN
         and Sal_Id = idSala
         and 
         (
-         diasAtras is null or  Ses_Fecha >= DATE_SUB(CURRENT_DATE(), INTERVAL diasAtras DAY)
+            diasAtras is null or Ses_Fecha >= DATE_SUB(CURRENT_DATE(), INTERVAL diasAtras DAY)
         )
     ORDER BY Ses_Fecha DESC, Ses_HoraInicio DESC;
 END &&
-CALL sp_monVerHistorialPc('A',1234, null);
 
 
-
--- Inhabilitar o bloquear computador y cerrar sesión (Utiliza Vista 1)
--- Atributos: inhabilitar (defecto false), sala, computador
-
-DROP PROCEDURE IF EXISTS sp_compHabilitarInhabilitar;
-DELIMITER &&
--- Ver sesiones en curso (Vista 1)
+-- Inhabilitar o bloquear computador y cerrar sesión
+DROP PROCEDURE IF EXISTS sp_compHabilitarInhabilitar &&
 CREATE PROCEDURE sp_compHabilitarInhabilitar(
     IN inhabilitar tinyint, IN idSala char(1), IN idComputador int
 )
@@ -128,69 +109,44 @@ BEGIN
     WHERE (
         Com_Id=idComputado and Sal_Id=idSala
     );
-    
-    -- Verificar
-    SELECT * FROM vw_Sesiones_En_Curso;
-    SELECT * FROM Computador WHERE Com_Id=@computador;
-    
 END &&
 
 -- Agregar comentario a una sesión
--- Atributos: fecha, hora_inicio, computador, sala, comentario
-DROP PROCEDURE IF EXISTS sp_monVerSesionesActuales;
-DELIMITER &&
--- Ver sesiones en curso (Vista 1)
-CREATE PROCEDURE sp_monVerSesionesActuales(
+DROP PROCEDURE IF EXISTS sp_agregarComentarioSesion &&
+CREATE PROCEDURE sp_agregarComentarioSesion(
     IN fechaSesion date, IN horaInicial time, IN idComputador int,
     IN idSala char(1), IN comentario varchar(45)
 )
 BEGIN
-    
     UPDATE Sesion SET Sal_Comentarios=comentario
     WHERE Ses_Fecha=fechaSesion and Ses_HoraInicio=horaInicial and Com_Id=idCmputador and Sal_Id=idSala;
-    
-    -- Verificar
-    SELECT * FROM Sesion
-    WHERE Ses_Fecha=fechaSesion and Ses_HoraInicio=horaInicial and Com_Id=idCmputador and Sal_Id=idSala;
-
-   
 END &&
 
-
-
-
-
+DELIMITER ;
 
 -- // ==========================================
 -- // Reserva de salas 
 -- // ==========================================
 
+DELIMITER &&
+
 -- Buscar empleado en el sistema
--- Atributos (todos opcionales): tiun_empleado, nombre_empleado, apellido_empleado, correo_empleado
-
-DROP PROCEDURE IF EXISTS sp_buscarEmpleado;
-DELIMITER &&
-	CREATE PROCEDURE sp_buscarEmpleado(
-		IN tiunEmpleado int, IN nombreEmpleado varchar(45), IN apellidoEmpleado varchar(45),
-		IN correoEmpleado varchar(45)
-	)
-	BEGIN
-		 SELECT *
-		FROM Empleado
-		WHERE
-			(tiunEmpleado is null or Emp_Tiun LIKE CONCAT(tiunEmpleado, '%'))
-			and (nombreEmpleado is null or Emp_Nombre LIKE CONCAT('%', nombreEmpleado, '%'))
-			and (apellidoEmpleado is null or Emp_Apellido LIKE CONCAT('%', apellidoEmpleado, '%'))
-			and (correoEmpleado is null or Emp_Correo LIKE CONCAT(correoEmpleado, '%'));
-
-
+DROP PROCEDURE IF EXISTS sp_buscarEmpleado &&
+CREATE PROCEDURE sp_buscarEmpleado(
+    IN tiunEmpleado int, IN nombreEmpleado varchar(45), IN apellidoEmpleado varchar(45),
+    IN correoEmpleado varchar(45)
+)
+BEGIN
+    SELECT *
+    FROM Empleado
+    WHERE
+        (tiunEmpleado is null or Emp_Tiun LIKE CONCAT(tiunEmpleado, '%'))
+        and (nombreEmpleado is null or Emp_Nombre LIKE CONCAT('%', nombreEmpleado, '%'))
+        and (apellidoEmpleado is null or Emp_Apellido LIKE CONCAT('%', apellidoEmpleado, '%'))
+        and (correoEmpleado is null or Emp_Correo LIKE CONCAT(correoEmpleado, '%'));
 END &&
--- Inserción de nuevos empleados
--- Atributos: tiun_empleado, nombre_empleado, apellido_empleado, correo_empleado, docente_planta, funcionario_cargo, facultad_codigo
-DROP PROCEDURE IF EXISTS sp_monVerSesionesActuales;
-DELIMITER &&
 
--- Ver sesiones en curso (Vista 1)
+-- Inserción de nuevos empleados
 CREATE PROCEDURE sp_monNuevoEmpleado(
     IN tiunEmpleado int, IN nombreEmpleado varchar(45), IN apellidoEmpleado varchar(45),
     IN correoEmpleado varchar(45), IN docentePlanta tinyint, IN funcionarioCargo varchar(45),
@@ -198,16 +154,30 @@ CREATE PROCEDURE sp_monNuevoEmpleado(
 )
 BEGIN
     INSERT INTO Empleado VALUES(tiunEmpleado, nombreEmpleado, apellidoEmpleado, correoEmpleado, docentePlanta, funcionarioCargo, facultadCodigo);
-    
-    -- Verificar
-    SELECT * FROM Empleado WHERE Emp_Tiun=tiunEmpleado;
-    
 END &&
 
-DELIMITER &&
+-- Actualización datos empleados
+CREATE PROCEDURE sp_monEditarEmpleado(
+    IN tiunEmpleado int, IN nombreEmpleado varchar(45), IN apellidoEmpleado varchar(45),
+    IN correoEmpleado varchar(45), IN docentePlanta tinyint, IN funcionarioCargo varchar(45),
+    IN facultadCodigo varchar(70)
+)
+BEGIN
+    IF tiunEmpleado is not null THEN
+        UPDATE Empleado
+        SET 
+            Emp_Nombre   = COALESCE(nombreEmpleado, Emp_Nombre),
+            Emp_Apellido = COALESCE(apellidoEmpleado, Emp_Apellido),
+            Emp_Correo   = COALESCE(correoEmpleado, Emp_Correo),
+            Doc_Planta   = COALESCE(docentePlanta, Doc_Planta),
+            Fun_Cargo    = COALESCE(funcionarioCargo, Fun_Cargo),
+            Fac_Codigo   = COALESCE(facultadCodigo, Fac_Codigo)
+        WHERE Emp_Tiun = tiunEmpleado;
+    END IF;
+END &&
 
 -- Ver reservas filtradas (Vista 6)
-DROP PROCEDURE IF EXISTS sp_verReservas;
+DROP PROCEDURE IF EXISTS sp_verReservas &&
 CREATE PROCEDURE sp_verReservas(
     IN dia date, IN horaInicial time, IN horaFinal time, IN idSala char(1),
     IN tiunEmpleado bigint, IN nombreEmpleado varchar(45), apellidoEmpleado varchar(45), IN correoEmpleado varchar(45),
@@ -226,7 +196,7 @@ BEGIN
         and (apellidoEmpleado is null or Emp_Apellido LIKE CONCAT('%', apellidoEmpleado, '%'))
         and (correoEmpleado is null or Emp_Correo LIKE CONCAT(correoEmpleado, '%'))
         and (estadoReserva is null or Estado_Actividad=estadoReserva);
-END
+END &&
 
 -- Agregar reserva
 DROP PROCEDURE IF EXISTS sp_agregarReserva;
