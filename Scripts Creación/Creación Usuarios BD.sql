c457bdaf50fc7cc7b3187d6e0f21c8f9e9e6f4ac
AngelaPuzzle <71221709+angelapuzzle@users.noreply.github.com>
2025-12-08 23:24:50 -0500

    Resulta que tocaba especificar el esquema en los permisos, ah y actualicé el CRUD, ahora es ECRUD

--- Scripts Creación/Creación Usuarios BD.sql
+++ Scripts Creación/Creación Usuarios BD.sql
@@ -1,46 +1,46 @@
 -- CREACIÓN DEL USUARIO ESTUDIANTE
     create user	'Estudiante'@'localhost' identified by 'estudiante';
 
-    grant execute on procedure sp_nuevaSesion to 'Estudiante'@'localhost';
-    grant execute on procedure sp_cerrarSesion to 'Estudiante'@'localhost';
+    grant execute on procedure sim_ba.sp_nuevaSesion to 'Estudiante'@'localhost';
+    grant execute on procedure sim_ba.sp_cerrarSesion to 'Estudiante'@'localhost';
     grant select, update on sim_ba.vw_com_disp to 'Estudiante'@'localhost';
     
 
 -- CREACIÓN DEL USUARIO MONITOR
     create user 'Monitor 1'@'localhost' identified by 'securepass1';
 
-    grant execute on procedure sp_verSesionesActuales to 'Monitor1'@'localhost';
-    grant execute on procedure sp_verSesionesHistorial to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_verSesionesActuales to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_verSesionesHistorial to 'Monitor1'@'localhost';
     grant select on sim_ba.vw_Ocupacion_Diaria to 'Monitor 1'@'localhost';
-    grant execute on procedure sp_monVerEstadisticasComp to 'Monitor1'@'localhost';
-    grant execute on procedure sp_monVerHistorialPc to 'Monitor1'@'localhost';
-    grant execute on procedure sp_compHabilitarInhabilitar to 'Monitor1'@'localhost';
-    grant execute on procedure sp_agregarComentarioSesion to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_monVerEstadisticasComp to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_monVerHistorialPc to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_compHabilitarInhabilitar to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_agregarComentarioSesion to 'Monitor1'@'localhost';
 
-    grant execute on procedure sp_buscarEmpleado to 'Monitor1'@'localhost';
-    grant execute on procedure sp_monNuevoEmpleado to 'Monitor1'@'localhost';
-    grant execute on procedure sp_monEditarEmpleado to 'Monitor1'@'localhost';
-    grant execute on procedure sp_verReservas to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_buscarEmpleado to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_monNuevoEmpleado to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_monEditarEmpleado to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_verReservas to 'Monitor1'@'localhost';
     grant select on sim_ba.vw_Ocupacion_Salas to 'Monitor 1'@'localhost';
-    grant execute on procedure sp_agregarReserva to 'Monitor1'@'localhost';
-    grant execute on procedure sp_cancelarReserva to 'Monitor1'@'localhost';
-    grant execute on procedure sp_deshabilitarSala to 'Monitor1'@'localhost';
-    grant execute on procedure sp_habilitarSala to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_agregarReserva to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_cancelarReserva to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_deshabilitarSala to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_habilitarSala to 'Monitor1'@'localhost';
 
-    grant execute on function fn_monitorLogin to 'Monitor1'@'localhost';
-    grant execute on procedure sp_monitorLogout to 'Monitor1'@'localhost';
+    grant execute on function sim_ba.fn_monitorLogin to 'Monitor1'@'localhost';
+    grant execute on procedure sim_ba.sp_monitorLogout to 'Monitor1'@'localhost';
 
     
 -- CREACIÓN DEL USUARIO ADMINISTRADOR
     CREATE USER 'Administrador'@'localhost' IDENTIFIED BY 'admin_secure_pass_2025';
     
-    grant execute on procedure sp_verTurnosMonitores to 'Administrador'@'localhost';
-    grant execute on procedure sp_verSupervisiones to 'Administrador'@'localhost';
-    grant execute on procedure sp_verSupervisados to 'Administrador'@'localhost';
+    grant execute on procedure sim_ba.sp_verTurnosMonitores to 'Administrador'@'localhost';
+    grant execute on procedure sim_ba.sp_verSupervisiones to 'Administrador'@'localhost';
+    grant execute on procedure sim_ba.sp_verSupervisados to 'Administrador'@'localhost';
 
-    grant execute on procedure sp_adminLogin to 'Administrador'@'localhost';
-    grant execute on procedure sp_adminLogout to 'Administrador'@'localhost';
-    grant execute on procedure sp_verTurnosAdmins to 'Administrador'@'localhost';
+    grant execute on procedure sim_ba.sp_adminLogin to 'Administrador'@'localhost';
+    grant execute on procedure sim_ba.sp_adminLogout to 'Administrador'@'localhost';
+    grant execute on procedure sim_ba.sp_verTurnosAdmins to 'Administrador'@'localhost';
 
     
 flush privileges;
